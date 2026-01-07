853e9e117dceee33d73d92f11c765f0d302b0bb9
eprad0 <b2029ejbprado@pshs.edu.ph>
2026-01-07 17:41:46 +0800

    commit

--- escobar-tracker/js/config.js
+++ escobar-tracker/js/config.js
@@ -1,4 +1,14 @@
 // 1) Paste your Firebase web app config here (from Firebase Console)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvG4xhDCRSrE4UvVc3OsQBVYNO7wfSW_g",
+  authDomain: "escobar-tracker.firebaseapp.com",
+  projectId: "escobar-tracker",
+  storageBucket: "escobar-tracker.firebasestorage.app",
+  messagingSenderId: "164794964142",
+  appId: "1:164794964142:web:a96991184dca5b14fdf53f"
+};
+
 export const firebaseConfig = {
   apiKey: "PASTE_ME",
   authDomain: "PASTE_ME",
